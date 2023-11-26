2d8bbbcc71bbf53580a495514314e24c12d2b810
Wagner <wagnerinfo@hotmail.com>
2023-11-26 08:13:46 -0300

    Versão 0.0.12 Google Chart - Fix Errors 3

--- resources/js/admin/components/ChartComponent.vue
+++ resources/js/admin/components/ChartComponent.vue
@@ -3,7 +3,7 @@
         <div class="col-12">
             <GChart
                 type="PieChart"
-                :data="this.googleChartData"
+                :data="JSON.parse(JSON.stringify(this.googleChartData))"
                 :options="chartOptions"
                 :width="400"
                 :height="300"
@@ -14,6 +14,7 @@
 
   <script>
   import { GChart } from 'vue-google-charts';
+
 
   export default {
     components: {
@@ -32,7 +33,7 @@
 
     data() {
       return {
-        googleChartData: null ,
+        googleChartData:  [] ,
         chartOptions: {
           title: this.title,
           is3D: true,
@@ -40,11 +41,17 @@
       };
     },
 
-    created(){
+    computed: {
+        getGoogleChartData() {
+            return JSON.parse(JSON.stringify(this.googleChartData));
+        }
+    },
+
+    mounted(){
         const jsonData = JSON.parse(this.responses);
-        const googleChartData = Object.values(jsonData).map(item => [item.text, item.count_value]);
-        googleChartData.unshift(['Resposta', 'Quantidade']);
-        this.$data.googleChartData = googleChartData ;
+        const tmp = Object.values(jsonData).map(item => [item.text, item.count_value]);
+        tmp.unshift(['Resposta', 'Quantidade']);
+        this.googleChartData = Array.from(tmp);
     }
   };
   </script>
