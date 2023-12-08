2c4545f20efcd74bae171c24d59bb49db85b5b3b
Wagner <wagnerinfo@hotmail.com>
2023-12-08 17:28:46 -0300

    Version 0.0.16 Create Gabarito

--- resources/views/ensalament.blade.php
+++ resources/views/ensalament.blade.php
@@ -50,7 +50,7 @@
                 @if(isset($data['selective_processes']) && ($data['selective_processes'] !== NULL))
                 <a href="{{ asset('login'); }}" class="get-started-btn">INSCREVA-SE</a>
                 @else
-                <a href="{{ asset('ensalamento'); }}" class="get-started-btn">ENSALAMENTOS</a>
+                <a href="{{ asset('ensalamento'); }}" class="get-started-btn">RESULTADOS</a>
                 @endif
               </div>
             </header><!-- End Header -->
@@ -65,7 +65,7 @@
                   <div class="row">
                         <div class="col-12">
 
-                            <h1>ENSALAMENTOS</h1>
+                            <h1>RESULTADOS</h1>
                             <hr class="pb-2">
 
                             @if(isset($data['ensalaments']) && count(['ensalaments']))
